94fb44313b68510175eabac75e531580c2a355f6
Ann He <annhe@Anns-MacBook-Pro.local>
2021-04-22 12:45:21 -0700

    replay buffer infra

--- code/utils.py
+++ code/utils.py
@@ -1,6 +1,17 @@
 import numpy as np 
 import random
 
+def sample_n_unique(sampling_f, n):
+    """Helper function. Given a function `sampling_f` that returns
+    comparable objects, sample n such unique objects.
+    """
+    res = []
+    while len(res) < n:
+        candidate = sampling_f()
+        if candidate not in res:
+            res.append(candidate)
+    return res
+
 class ReplayBuffer(object):
 	raise NotImplementedError
 	self.num_in_buffer = 0
@@ -8,3 +19,57 @@ class ReplayBuffer(object):
 	def can_sample(self, batch_size):
         """Returns true if `batch_size` different transitions can be sampled from the buffer."""
         return batch_size + 1 <= self.num_in_buffer
+
+    def store_observation(self, state):
+    	raise NotImplementedError
+
+    def store_effect(self, a, r, s_next, done):
+    	"""
+    	Store effect of taking an action
+    	"""
+    	raise NotImplementedError
+
+    """
+    To-Do: Modify this
+    """
+    def _encode_sample(self, idxes):
+        obs_batch      = np.concatenate([self._encode_observation(idx)[None] for idx in idxes], 0)
+        act_batch      = self.action[idxes]
+        rew_batch      = self.reward[idxes]
+        next_obs_batch = np.concatenate([self._encode_observation(idx + 1)[None] for idx in idxes], 0)
+        done_mask      = np.array([1.0 if self.done[idx] else 0.0 for idx in idxes], dtype=np.float32)
+
+        return obs_batch, act_batch, rew_batch, next_obs_batch, done_mask
+
+    def sample(self, batch_size):
+        """Sample `batch_size` different transitions.
+        i-th sample transition is the following:
+        when observing `obs_batch[i]`, action `act_batch[i]` was taken,
+        after which reward `rew_batch[i]` was received and subsequent
+        observation  next_obs_batch[i] was observed, unless the epsiode
+        was done which is represented by `done_mask[i]` which is equal
+        to 1 if episode has ended as a result of that action.
+        Parameters
+        ----------
+        batch_size: int
+            How many transitions to sample.
+        Returns
+        -------
+        obs_batch: np.array
+            Array of shape
+            (batch_size, img_h, img_w, img_c * frame_history_len)
+            and dtype np.uint8
+        act_batch: np.array
+            Array of shape (batch_size,) and dtype np.int32
+        rew_batch: np.array
+            Array of shape (batch_size,) and dtype np.float32
+        next_obs_batch: np.array
+            Array of shape
+            (batch_size, img_h, img_w, img_c * frame_history_len)
+            and dtype np.uint8
+        done_mask: np.array
+            Array of shape (batch_size,) and dtype np.float32
+        """
+        assert self.can_sample(batch_size)
+        idxes = sample_n_unique(lambda: random.randint(0, self.num_in_buffer - 2), batch_size)
+        return self._encode_sample(idxes)
